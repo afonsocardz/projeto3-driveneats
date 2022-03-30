8537bf888878325945949c573f97d3e5e7e07a11
afonsocardz <afonso.desenvolvimento@gmail.com>
2022-03-29 23:49:27 -0300

    Estrutura da tela de confirmação e início do CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-        1 arquivo(s) copiado(s).
+let touchstartX = 0
+let touchendX = 0
+
+const slider = document.getElementById('slider')
+
+function handleGesture() {
+  if (touchendX < touchstartX) alert('swiped left!')
+  if (touchendX > touchstartX) alert('swiped right!')
+}
+
+slider.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+slider.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  handleGesture()
+})
